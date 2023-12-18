6c69e8b9ed35bd880eae75681d633d45a434c288
bacan007 <yesidelbacan.yc@gmail.com>
2023-12-18 15:23:33 -0500

    404

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {  Link } from "react-router-dom";
 
 const PageNotFound = () => (
   <section className="PageNotFound">
-    <h3>Hello PageNotFound</h3>
+    <div className='text-center mt-5'>
+      <img src={window.location.origin + '/404.png'} className='img-fluid img-thumbnail'></img>
+    </div>
+    <div className='text-center'>
+      <Link className="btn btn-info btn btn-lg  mt-5" to={`/`}>Volver al inicio</Link>
+    </div>
   </section>
 )
 
